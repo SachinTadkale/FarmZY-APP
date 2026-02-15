56578676e618cc5eab48fbdf260be5ffb962d011
Sachin Tadkale <sachintadkale9960@gmail.com>
2026-02-15 15:24:59 +0530

    updated apk files

---
+++ lib/core/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FarmZY extends StatelessWidget {
+  const FarmZY({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FarmZY',
+    );
+  }
+}
